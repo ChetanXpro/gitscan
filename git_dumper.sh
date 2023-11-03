6f0078026665d61444338063d34b52302570204c
chetan <chetan@chetans-MacBook-Air.local>
2023-11-03 18:50:11 +0530

    minor fix

--- git_dumper.sh
+++ git_dumper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# function to process index files
+# Function to process index files
 process_indexes () {
-    find .git/objects/pack/ -name "*.idx" | while read idx_file; do
+    find .git/objects/pack/ -name "*.idx" | while read -r idx_file; do
         git show-index < "$idx_file" | awk '{print $2}'
     done
 }
 
-# function to process non-pack objects
+# Function to process non-pack objects
 process_objects () {
     find .git/objects/ -type f | grep -v '/pack/' | awk -F'/' '{print $(NF-1)$NF}'
 }
@@ -16,12 +16,6 @@ process_objects () {
 all_objects=$( { process_indexes; process_objects; } )
 
 # Iterate over all objects and pretty print their contents
-for object in $all_objects; do
-    git cat-file -p $object
-done
-
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
-~                                                                                   
+while IFS= read -r object; do
+    git cat-file -p "$object"
+done <<< "$all_objects"
